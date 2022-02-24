caa23f74c9d9d33170c9323b33f75bc14b53f7d5
Sidney N <sidneynezz@gmail.com>
2022-02-24 18:36:00 -0500

    Started Array Activity (js)

---
+++ itis3135/scripts/array.js
@@ -0,0 +1,12 @@
+var person = ["Layne Bryant", "Herbert Jacobs", "Renee Ballad", "Lily Walker", "Anayeli Anaya", "Oya Tammu", "Jasmine Gentry", "Herbert Jacobs", "Taylor Richardson"];
+var saleries = [86000, 93000, 82500, 92000, 87000, 92000, 96000, 101000, 98000];
+
+function addSalary(){
+
+}
+function displayResults(){
+    document.getElementById("results").innerHTML = ("The average salary is ") + average (". The highest salary is ") + highest + (".")
+}
+function displaySalary(){
+
+}
