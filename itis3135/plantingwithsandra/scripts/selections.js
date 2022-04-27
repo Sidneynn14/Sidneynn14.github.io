b973f218c459b29da09d192e6f449eff5ae60679
Sidney N <sidneynezz@gmail.com>
2022-04-27 01:26:54 -0400

    choosingplants page update

---
+++ itis3135/plantingwithsandra/scripts/selections.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $("#nav_list a").click(function (event) {
+      console.log("! list element clicked");
+      plantfile = $(this).attr("title") + ".json";
+      console.log(plantfile);
+      $.getJSON("json_files/plants/" + plantfile, function (data) {
+        $.each(data, function () {
+          $.each(this, function (key, value) {  
+            $("main h1").remove();
+            $("main h2").remove();
+            $("main h3").remove();
+            $("main img").remove();
+            $("main p").remove();
+            $("main").append("<h1>" + value.long_official_name + "</h2>" + "<h2>" + value.name + "</h2>" + "<h3>" + value.nickname + "</h3>" + "<img src=" + value.image + ">" + "<p>" + value.text + "</p>");
+          });
+        });
+      });
+    });
+    $.ajax({type: "get",url: ""});
+}); 
